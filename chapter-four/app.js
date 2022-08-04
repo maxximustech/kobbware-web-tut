b57f790dd0ae6ed178985b4031cb212e8e61c3b3
Maxximus <abdullahiazeezbolu@gmail.com>
2022-08-04 15:07:35 +0100

    Working with express js

--- chapter-four/app.js
+++ chapter-four/app.js
@@ -1,17 +1,79 @@
 const http = require('http');
+const exp = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
-const server = http.createServer(function(req,res){
-    res.statusCode = 200;
-    res.write('<html>');
-    res.write('<head><title>Node Server</title></head>');
-    if(req.url === '/'){
-        res.write('<body><h1>HomePage!</h1></body>');
-    }else if(req.url === '/about'){
-        res.write('<body><h1>About Page!</h1></body>');
-    }else{
-        res.write('<body><h1>Other Page!</h1></body>');
-    }
-    res.write('</html>');
-    res.end();
+const app = exp();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'html','login.html'));
 });
+app.post('/login',(req,res)=>{
+    let users = fs.readFileSync('data.json');
+    let parsedUsers = JSON.parse(users);
+    let user = parsedUsers.filter(u => {
+        return u.username.toLowerCase() === req.body.username.toLowerCase();
+    });
+    if(user.length <= 0){
+        console.log('User does not exist');
+        res.redirect('/login');
+        return;
+    }
+    if(user[0].pass.toString() !== req.body.password.toString()){
+        console.log('Password not correct');
+        res.redirect('/login');
+        return;
+    }
+    res.redirect('/');
+});
+
+app.get('/signup',(req,res)=>{
+    res.sendFile(path.join(__dirname,'html','signup.html'));
+});
+
+app.post('/signup',(req,res, next)=>{
+    let user = req.body;
+    let users = fs.readFileSync('data.json');
+    let parsedUsers = JSON.parse(users);
+    let check = parsedUsers.filter(u => {
+        return u.username.toLowerCase() === user.username.toLowerCase();
+    });
+    if(check.length > 0){
+        console.log('Username already exist');
+        res.redirect('/signup');
+        return;
+    }
+    parsedUsers.push({
+        username: user.username,
+        pass: user.password
+    });
+    fs.writeFileSync('data.json',JSON.stringify(parsedUsers));
+    res.redirect('/login');
+});
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'html','dom.html'));
+});
+
+app.use((req,res)=>{
+    res.status(404);
+    res.sendFile(path.join(__dirname,'html','404.html'));
+});
+
+/*app.use((req, res, next)=>{
+    //console.log(path.join(__dirname,'html','dom.html'));
+    //res.sendFile(__dirname+'/html/dom.html');
+    res.status(200);
+    //res.sendFile(path.join(__dirname,'html','dom.html'));
+    res.json({
+        title: 'Node Project',
+        description: 'A complete tutorial on node js'
+    });
+});
+
+ */
+
+const server = http.createServer(app);
 server.listen(4000);
